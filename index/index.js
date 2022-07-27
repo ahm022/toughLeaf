7bdcc5b2b6957f4388bb6dd3b44c357f3273a2d0
Mariam Suleiman <msuleiman@Netways.com>
2022-07-27 18:01:06 +0300

    introduction section

---
+++ index/index.js
@@ -0,0 +1,51 @@
+const xdbControl = document.getElementById('xdb-control');
+const propjectsControl = document.getElementById('propjects-control');
+const postControl = document.getElementById('post-control');
+
+const defaultState = document.getElementById('default-state');
+const xbeDb = document.getElementById('xbe-db');
+const availableProjects = document.getElementById('available-projects');
+const postProject = document.getElementById('post-project');
+
+
+xdbControl?.addEventListener('mouseover', function handleMouseOver() {
+    defaultState.style.display = 'none';
+    availableProjects.style.display = 'none';
+    postProject.style.display = 'none';
+    xbeDb.style.display = 'block';
+});
+
+xdbControl?.addEventListener('mouseout', function handleMouseOver() {
+    defaultState.style.display = 'block';
+    availableProjects.style.display = 'none';
+    postProject.style.display = 'none';
+    xbeDb.style.display = 'none';
+});
+
+propjectsControl?.addEventListener('mouseover', function handleMouseOver() {
+    defaultState.style.display = 'none';
+    availableProjects.style.display = 'block';
+    postProject.style.display = 'none';
+    xbeDb.style.display = 'none';
+});
+
+propjectsControl?.addEventListener('mouseout', function handleMouseOver() {
+    defaultState.style.display = 'block';
+    availableProjects.style.display = 'none';
+    postProject.style.display = 'none';
+    xbeDb.style.display = 'none';
+});
+
+postControl?.addEventListener('mouseover', function handleMouseOver() {
+    defaultState.style.display = 'none';
+    availableProjects.style.display = 'none';
+    postProject.style.display = 'block';
+    xbeDb.style.display = 'none';
+});
+
+postControl?.addEventListener('mouseout', function handleMouseOver() {
+    defaultState.style.display = 'block';
+    availableProjects.style.display = 'none';
+    postProject.style.display = 'none';
+    xbeDb.style.display = 'none';
+});
