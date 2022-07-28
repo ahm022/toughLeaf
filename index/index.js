fc95f4ee05f74751fae6ed86e05410ae1415ba8f
Mariam Suleiman <msuleiman@Netways.com>
2022-07-28 11:48:16 +0300

    section four

--- index/index.js
+++ index/index.js
@@ -8,6 +8,31 @@ const availableProjects = document.getElementById('available-projects');
 const postProject = document.getElementById('post-project');
 
 
+const hiring = document.getElementById('hiring');
+const xbe = document.getElementById('xbe');
+const bgImage = document.getElementById("bg-img");
+
+hiring?.addEventListener('mouseover', function handleMouseOver() {
+    xbe.style.opacity = '0.5';
+    bgImage.src = "../img/section-four/hiring-bg.png"
+});
+
+hiring?.addEventListener('mouseout', function handleMouseOver() {
+    xbe.style.opacity = '1';
+    bgImage.src = "../img/section-four/mask-group-upper.png"
+});
+
+xbe?.addEventListener('mouseover', function handleMouseOver() {
+    hiring.style.opacity = '0.5';
+    bgImage.src = "../img/section-four/xbe-bg.png"
+});
+
+xbe?.addEventListener('mouseout', function handleMouseOver() {
+    hiring.style.opacity = '1';
+    bgImage.src = "../img/section-four/mask-group-upper.png";
+});
+
+
 xdbControl?.addEventListener('mouseover', function handleMouseOver() {
     defaultState.style.display = 'none';
     availableProjects.style.display = 'none';
