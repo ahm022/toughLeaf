876ff460591a1b111d3a5dab2a7bdec8b2b3d1c9
Mariam Suleiman <msuleiman@Netways.com>
2022-07-29 14:54:46 +0300

    section seven

--- index/index.js
+++ index/index.js
@@ -13,102 +13,116 @@ const xbe = document.getElementById('xbe');
 const bgImage = document.getElementById("bg-img");
 
 hiring?.addEventListener('mouseover', function handleMouseOver() {
-    xbe.style.opacity = '0.5';
-    bgImage.src = "../img/section-four/hiring-bg.png"
+  xbe.style.opacity = '0.5';
+  bgImage.src = "../img/section-four/hiring-bg.png"
 });
 
 hiring?.addEventListener('mouseout', function handleMouseOver() {
-    xbe.style.opacity = '1';
-    bgImage.src = "../img/section-four/mask-group-upper.png"
+  xbe.style.opacity = '1';
+  bgImage.src = "../img/section-four/mask-group-upper.png"
 });
 
 xbe?.addEventListener('mouseover', function handleMouseOver() {
-    hiring.style.opacity = '0.5';
-    bgImage.src = "../img/section-four/xbe-bg.png"
+  hiring.style.opacity = '0.5';
+  bgImage.src = "../img/section-four/xbe-bg.png"
 });
 
 xbe?.addEventListener('mouseout', function handleMouseOver() {
-    hiring.style.opacity = '1';
-    bgImage.src = "../img/section-four/mask-group-upper.png";
+  hiring.style.opacity = '1';
+  bgImage.src = "../img/section-four/mask-group-upper.png";
 });
 
 
 xdbControl?.addEventListener('mouseover', function handleMouseOver() {
-    defaultState.style.display = 'none';
-    availableProjects.style.display = 'none';
-    postProject.style.display = 'none';
-    xbeDb.style.display = 'flex';
+  defaultState.style.display = 'none';
+  availableProjects.style.display = 'none';
+  postProject.style.display = 'none';
+  xbeDb.style.display = 'flex';
 });
 
 xdbControl?.addEventListener('mouseout', function handleMouseOver() {
-    defaultState.style.display = 'block';
-    availableProjects.style.display = 'none';
-    postProject.style.display = 'none';
-    xbeDb.style.display = 'none';
+  defaultState.style.display = 'block';
+  availableProjects.style.display = 'none';
+  postProject.style.display = 'none';
+  xbeDb.style.display = 'none';
 });
 
 propjectsControl?.addEventListener('mouseover', function handleMouseOver() {
-    defaultState.style.display = 'none';
-    availableProjects.style.display = 'flex';
-    postProject.style.display = 'none';
-    xbeDb.style.display = 'none';
+  defaultState.style.display = 'none';
+  availableProjects.style.display = 'flex';
+  postProject.style.display = 'none';
+  xbeDb.style.display = 'none';
 });
 
 propjectsControl?.addEventListener('mouseout', function handleMouseOver() {
-    defaultState.style.display = 'block';
-    availableProjects.style.display = 'none';
-    postProject.style.display = 'none';
-    xbeDb.style.display = 'none';
+  defaultState.style.display = 'block';
+  availableProjects.style.display = 'none';
+  postProject.style.display = 'none';
+  xbeDb.style.display = 'none';
 });
 
 postControl?.addEventListener('mouseover', function handleMouseOver() {
-    defaultState.style.display = 'none';
-    availableProjects.style.display = 'none';
-    postProject.style.display = 'flex';
-    xbeDb.style.display = 'none';
+  defaultState.style.display = 'none';
+  availableProjects.style.display = 'none';
+  postProject.style.display = 'flex';
+  xbeDb.style.display = 'none';
 });
 
 postControl?.addEventListener('mouseout', function handleMouseOver() {
-    defaultState.style.display = 'block';
-    availableProjects.style.display = 'none';
-    postProject.style.display = 'none';
-    xbeDb.style.display = 'none';
+  defaultState.style.display = 'block';
+  availableProjects.style.display = 'none';
+  postProject.style.display = 'none';
+  xbeDb.style.display = 'none';
 });
 
 //  swiper
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-  var swiperOne = new Swiper(".swiperOne", {
-    
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  });
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+var swiperOne = new Swiper(".swiperOne", {
 
-  var infoSwiper = new Swiper(".infoSwiper", {
-    pagination: {
-        el: ".swiper-pagination-info", 
-    }
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var infoSwiper = new Swiper(".infoSwiper", {
+  pagination: {
+    el: ".swiper-pagination-info",
+  }
+});
 
 // auth  
-const displayLoggedInPage = ()=>{
-    hideRegisterPage()
-    document.getElementById('signIn').classList.add("show")
+const displayLoggedInPage = () => {
+  hideRegisterPage()
+  document.getElementById('signIn').classList.add("show")
+}
+const hideLoggedInPage = () => {
+  document.getElementById('signIn').classList.remove("show")
+}
+const displayRegisterPage = () => {
+  hideLoggedInPage()
+  document.getElementById('signUp').classList.add("show")
+}
+const hideRegisterPage = () => {
+  document.getElementById('signUp').classList.remove("show")
+}
+
+const toggleCollapse = (id) => {
+  const span = document.getElementById(id);
+
+  let titleDiv = span.parentNode;
+  if (titleDiv.classList?.length > 1) {
+    titleDiv.classList.remove("blue-color");
+    span.textContent = '+';
   }
-  const hideLoggedInPage = ()=>{
-    document.getElementById('signIn').classList.remove("show")
+  else {
+    titleDiv.classList.add("blue-color");
+    span.textContent = '-';
   }
-  const displayRegisterPage = ()=>{
-    hideLoggedInPage ()
-    document.getElementById('signUp').classList.add("show")
-  }
-  const hideRegisterPage = ()=>{
-    document.getElementById('signUp').classList.remove("show")
-  }
-  
+
+}
